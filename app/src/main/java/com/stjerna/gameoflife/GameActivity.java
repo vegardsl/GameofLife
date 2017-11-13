a3cfee8caa5d15b0f84f9ddbaea1f7eb556e43e0
vegardsl <vegstjli@hotmail.com>
2017-11-13 23:02:22 +0100

    Cleaned up unnecessary import.

--- app/src/main/java/com/stjerna/gameoflife/GameActivity.java
+++ app/src/main/java/com/stjerna/gameoflife/GameActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
-import android.widget.TextView;
 
 import com.stjerna.gameoflife.conway.GameOfLifeImpl;
 
